7613641a79003bd032aef5806b080ba46cfb449e
Kavia AI <production@kavia.ai>
2025-09-03 05:33:07 +0000

    CheckPoint - cg9eb8bfaa

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/tic-tac-toe-classic-6480-6489/tic_tac_toe_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
